91ac637b71d42ae66910c3125bb1973a2fff91c2
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-05 17:10:58 +0530

    Learn about values & variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,20 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Sagar";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let person = "sagar";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+console.log(myCurrentJob)
