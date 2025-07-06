67465d4c8ad9e452b27f399f68ed1083f354588d
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-06 10:17:26 +0530

    Learn about truthy & falsy values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -162,3 +162,29 @@ let n = '1' + 1; // string concatinate 11
 n = n - 1; // - convert 11 - 1 (10)
 console.log(n);
 */
+
+
+/*
+// Truthy & falsy values.
+// 5 falsy values: 0, '', undefined, null, NaN;
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Sagar'));
+console.log(Boolean({}));
+console.log(Boolean(''))
+
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all ;");
+} else {
+  console.log('You should get a job!');
+}
+
+let height;
+if (height) {
+  console.log('YAY! Height is defined'); // height is undefined so that it is false
+} else {
+  console.log("Height is UNDEFINED"); // else is execute
+}
+*/
