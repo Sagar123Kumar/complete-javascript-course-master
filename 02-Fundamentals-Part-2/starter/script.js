42f8bb35f3a3b01f385b85a087a2a6549893f5ed
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-06 17:33:52 +0530

    Implementing Function definition & expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -30,3 +30,21 @@ console.log(orangeJuice);
 const num = Number('23');
 console.log(num);
 */
+
+/*
+// Function Declarations vs Expressions
+
+// Function declaration
+function calcAge1(birthYear) { // we can call function before function declaration 
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+//Function expression
+const calcAge2 = function (birthYear) { //we can't call function before function expression
+  return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+*/
