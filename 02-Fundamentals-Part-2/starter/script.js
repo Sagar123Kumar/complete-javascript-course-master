3bda12ff751fe3ffaae480855c52aed035684bfb
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-06 16:48:03 +0530

    Learn about functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,3 +7,26 @@ const passTest = true;
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
 */
+
+/*
+// Functions
+function logger() {
+  console.log('My name is Sagar');
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const orangeJuice = fruitProcessor(2, 4);
+console.log(orangeJuice);
+
+const num = Number('23');
+console.log(num);
+*/
