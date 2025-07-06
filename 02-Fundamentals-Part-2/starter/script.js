6c5cb0a088ddbd67e2eb02f825ae21a21f4a9051
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-06 21:22:32 +0530

    Implementing the different way of Array

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -113,3 +113,42 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 console.log(yearsUntilRetirement(1991, 'Sagar'));
 console.log(yearsUntilRetirement(1970, 'Rohan'));
 */
+
+/*
+// Introdunction to Arrays
+const friends = ['Micheal', 'Steven', 'Peter'];
+console.log(friends);
+
+// const years= new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Sagar';
+const sagar = [firstName, 'Kumar', 2037 - 1991, 'teacher', friends];
+
+console.log(sagar);
+console.log(sagar[4][0]);
+
+// Exercise 
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+// different way to use array argument as a function call (expression)
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+*/
