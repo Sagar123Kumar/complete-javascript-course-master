4d9ec97143c6eec2ff6e544b3463e4098ed59abf
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-09 08:10:32 +0530

    See the implementation for for loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -280,3 +280,10 @@ console.log(sagar);
 
 console.log(sagar.getSummary());
 */
+
+/*
+//Iteration The for loop
+for(let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weight repetition ${rep}`);
+}
+*/
