e63e914e8352c49639e114fa052277f5739dd46e
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-12 13:07:38 +0530

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/*
+// Selecting and Manupulating Elements
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!'
 console.log(document.querySelector('.message').textContent);
 
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 30;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
