22c51c619a0d16a327b795685ece78458781700d
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-12 13:46:52 +0530

    Learn Handling click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,3 +12,13 @@ document.querySelector('.score').textContent = 30;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+
+// Handling Click Events
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!'
+  }
+});
