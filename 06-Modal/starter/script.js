726516ba64098102976162cd021741d1bb49cff8
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-13 19:54:11 +0530

    Working with classes & continue our project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,8 +5,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+// function for open modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
+// function for close modal & overlay
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// running all the events
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 }
