2af04faecb79c324682f27627b5b1a12c642a722
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-14 19:20:18 +0530

    Added switching the active player functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -13,10 +15,10 @@ const btnHold = document.querySelector('.btn--hold');
 // Starting coditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-// diceEl.classList.remove('hidden');
-// Storing currentScore
+
+const score = [0, 0];
 let currentScore = 0;
-let checkCurrent = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -26,7 +28,7 @@ btnRoll.addEventListener('click', function () {
   // 2. Rolling dice
   diceEl.classList.add('rollAnm');
 
-  // 2.1 stop the rolling 
+  // 2.1 stop the rolling
   document.addEventListener('mousedown', function (e) {
     diceEl.classList.remove('rollAnm');
   });
@@ -39,8 +41,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // change later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
