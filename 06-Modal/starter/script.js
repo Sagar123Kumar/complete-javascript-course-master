b3c44f6f010c32db93dce9a6185257486666f655
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-14 08:44:37 +0530

    Complete the modal window project with all the features

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+// selecting all the classes that need to be selected
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
@@ -20,6 +22,16 @@ const closeModal = function () {
 // running all the events
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
-  btnCloseModal.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
 }
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e);
+
+  // Login to handling and Esc keypress event
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+*/
