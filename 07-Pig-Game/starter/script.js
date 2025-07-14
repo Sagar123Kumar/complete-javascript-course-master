da8b3a3e7d19811386fd9d5062541847c73f1b99
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-14 15:11:36 +0530

    Implement the concept of rolling the dice and other stuff in pig game project

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,10 +3,44 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting coditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// diceEl.classList.remove('hidden');
+// Storing currentScore
+let currentScore = 0;
+let checkCurrent = 0;
 
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Rolling dice
+  diceEl.classList.add('rollAnm');
+
+  // 2.1 stop the rolling 
+  document.addEventListener('mousedown', function (e) {
+    diceEl.classList.remove('rollAnm');
+  });
+
+  // 3. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1:
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // change later
+  } else {
+    // Switch to next player
+  }
+});
