81cb54f3723884774c4f6ea6babefbfd9ad6668d
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-26 21:01:02 +0530

    Learn about rest operator & parameter

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -64,7 +64,51 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  }
+
 };
+
+/*
+// 1) Destructruing
+// Array Resting
+// Spread, because on righ side of =
+const arr = [1, 2, ...[3, 4]];
+
+// Rest, because on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects Rest pattern
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// 2) Functions (...number) rest parameter
+const add = function (...numbers) {
+  let sum = 0;
+  for(let i = 0; i < numbers.length; i++) {
+    sum +=numbers[i];
+  }
+  console.log(sum);
+}
+add(2,3);
+add(5,3,7,2);
+add(8,2,5,3,2,1,4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushroom');
+*/
 
 /*
 // The Spred Operator
@@ -91,14 +135,15 @@ const str = 'Sagar';
 const letters = [...str, ' ', 'S.'];
 console.log(letters);
 console.log(...str);
+// invalid
 // console.log(`${...str}`)
 
 // Real-world examples
 const ingredients = [
   // prompt("Let's make pasta! Ingredient 1?"),
-  // prompt('Ingredient 2?'),
-  // prompt('Ingredient 3?'),
-];
+  // prompt('I'),
+];ngredient 2?'),
+  // prompt('Ingredient 3?
 console.log(ingredients);
 restaurant.orderPasta(...ingredients);
 
