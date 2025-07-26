a4e41c7a46a2c8b1fcfad0dd251e60a688dc4554
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-07-26 11:12:34 +0530

    Complete the module of behind the scenes working of js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -41,3 +41,27 @@ calcAge(1991);
 // console.log(age);
 // printAge();
 */
+
+// Regular fun vs ArrowFun
+
+const sagar = {
+  firstName: 'Sagar',
+  year: 1991,
+  clacAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    const self = this;
+    const isMillenal = function () {
+      // console.log(this.year >= 1981 && this.year <= 1996)
+      console.log(self.year >= 1981 && self.year <= 1996)
+    };
+    isMillenal();
+  },
+  greet: function () {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+sagar.greet();
+sagar.clacAge();
