87930f8c445117141a957d7ac80de4825229bb34
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2025-08-01 21:07:26 +0530

    See the methods and iteration over map

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -78,7 +78,46 @@ const restaurant = {
 };
 
 /*
-// Maps
+// Maps itteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again 🥲']
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// itteration
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer: '));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(answer === question.get('correct')));
+
+// Convert map to arrays
+console.log(...question);
+// methods
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+*/
+
+/*
+// Maps fundamental
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy');
